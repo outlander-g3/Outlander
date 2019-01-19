573362387afff688bf7398c48e8b531e9ea0ff21
SNM <dsabill555@gmail.com>
2019-01-19 19:25:42 +0800

    2019119

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
-// window.onload=()=>{
-//     var burger = new Vue({
-//         el:"b"
-//     })
-// }
+window.onload=()=>{
+    var web = window.innerWidth;
+    var burger = new Vue({
+        el:"#burger",
+        methods:{
+            bur(){
+                document.querySelector("#burgerText").style.left="0%";
+            }
+        }
+    })
+    var burgerText = new Vue({
+        el:"#burgerText",
+        data:{
+            web:web
+        },
+        methods:{
+            out(){
+                if(web<768){
+                    document.querySelector("#burgerText").style.left="-100%";
+                }
+            }
+        }
+    })
+}

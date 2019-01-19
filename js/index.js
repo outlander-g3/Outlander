12a229cdd15f920de5c8e8757f1fcf2f96c3b45d
SNM <dsabill555@gmail.com>
2019-01-20 00:00:03 +0800

    2019119

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ window.onload=()=>{
         methods:{
             bur(){
                 document.querySelector("#burgerText").style.left="0%";
+                document.querySelector("#burSpan1").classList.add("animation1");
+                document.querySelector("#burSpan2").classList.add("animation2");
+                document.querySelector("#burSpan3").classList.add("animation1");
             }
         }
     })
@@ -17,6 +20,9 @@ window.onload=()=>{
             out(){
                 if(web<768){
                     document.querySelector("#burgerText").style.left="-100%";
+                    document.querySelector("#burSpan1").classList.remove("animation1");
+                    document.querySelector("#burSpan2").classList.remove("animation2");
+                    document.querySelector("#burSpan3").classList.remove("animation1");
                 }
             }
         }

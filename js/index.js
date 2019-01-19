0f3447a86426a60cfc785c626ba5f95f562eebce
SNM <dsabill555@gmail.com>
2019-01-20 00:26:16 +0800

    2019120

--- js/index.js
+++ js/index.js
@@ -1,30 +1,3 @@
 window.onload=()=>{
-    var web = window.innerWidth;
-    var burger = new Vue({
-        el:"#burger",
-        methods:{
-            bur(){
-                document.querySelector("#burgerText").style.left="0%";
-                document.querySelector("#burSpan1").classList.add("animation1");
-                document.querySelector("#burSpan2").classList.add("animation2");
-                document.querySelector("#burSpan3").classList.add("animation1");
-            }
-        }
-    })
-    var burgerText = new Vue({
-        el:"#burgerText",
-        data:{
-            web:web
-        },
-        methods:{
-            out(){
-                if(web<768){
-                    document.querySelector("#burgerText").style.left="-100%";
-                    document.querySelector("#burSpan1").classList.remove("animation1");
-                    document.querySelector("#burSpan2").classList.remove("animation2");
-                    document.querySelector("#burSpan3").classList.remove("animation1");
-                }
-            }
-        }
-    })
+    
 }

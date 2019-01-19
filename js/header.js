1ebd5359b5c87059bec0156d8862be0f8aaf6adf
SNM <dsabill555@gmail.com>
2019-01-20 03:53:31 +0800

    2019120

--- js/header.js
+++ js/header.js
@@ -1,25 +1,27 @@
-window.onload=()=>{
+window.onload = () => {
     var web = window.innerWidth;
     var burger = new Vue({
-        el:"#burger",
-        methods:{
-            bur(){
-                document.querySelector("#burgerText").style.left="0%";
-                document.querySelector("#burSpan1").classList.add("animation1");
-                document.querySelector("#burSpan2").classList.add("animation2");
-                document.querySelector("#burSpan3").classList.add("animation1");
+        el: "#burger",
+        methods: {
+            bur() {
+                if (web < 768) {
+                    document.querySelector("#burgerText").style.left = "0%";
+                    document.querySelector("#burSpan1").classList.add("animation1");
+                    document.querySelector("#burSpan2").classList.add("animation2");
+                    document.querySelector("#burSpan3").classList.add("animation1");
+                }
             }
         }
     })
     var burgerText = new Vue({
-        el:"#burgerText",
-        data:{
-            web:web
+        el: "#burgerText",
+        data: {
+            web: web
         },
-        methods:{
-            out(){
-                if(web<768){
-                    document.querySelector("#burgerText").style.left="-100%";
+        methods: {
+            out() {
+                if (web < 768) {
+                    document.querySelector("#burgerText").style.left = "-100%";
                     document.querySelector("#burSpan1").classList.remove("animation1");
                     document.querySelector("#burSpan2").classList.remove("animation2");
                     document.querySelector("#burSpan3").classList.remove("animation1");
@@ -27,4 +29,21 @@ window.onload=()=>{
             }
         }
     })
+    var scr = window.scrollY;
+    var headerdAll = document.querySelector("#headerAll");
+    var burgerText = document.querySelector("#burgerText");
+    if(web>768){
+        window.onscroll = () => {
+            console.log("123");
+            if (scr < window.scrollY) {
+                headerdAll.style.top="-60px";
+                burgerText.style.top="-60px";
+            }
+            else if (scr > window.scrollY) {
+                headerdAll.style.top="0px";
+                burgerText.style.top="0px";
+            }
+            scr = window.scrollY;
+        }
+    }
 }

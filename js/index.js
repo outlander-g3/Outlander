c22f29703568d5b4fedc61721e89218a6a4fc45a
SNM <dsabill555@gmail.com>
2019-01-21 16:17:33 +0800

    2019121

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     var h1 = document.querySelector("h1");
-    h1.style.fontSize=((window.innerWidth)/10)+"px";
+    h1.style.fontSize = ((window.innerWidth) / 10) + "px";
     var searchicon = document.querySelector(".searchicon");
-    searchicon.style.fontSize=((window.innerWidth)/30)+"px";
+    searchicon.style.fontSize = ((window.innerWidth) / 30) + "px";
     var bannerSearch = document.querySelector("#bannerSearch");
-    bannerSearch.style.fontSize=((window.innerWidth)/30)+"px";
-    window.onresize=()=>{
-        h1.style.fontSize=((window.innerWidth)/10)+"px";
-        searchicon.style.fontSize=((window.innerWidth)/30)+"px";
-        bannerSearch.style.fontSize=((window.innerWidth)/30)+"px";
+    bannerSearch.style.fontSize = ((window.innerWidth) / 30) + "px";
+    window.onresize = () => {
+        h1.style.fontSize = ((window.innerWidth) / 10) + "px";
+        searchicon.style.fontSize = ((window.innerWidth) / 30) + "px";
+        bannerSearch.style.fontSize = ((window.innerWidth) / 30) + "px";
     }
-    for(let i = 1 ; i<=9;i++ ){
-        var indexName = document.querySelector("#indexName"+i);
-        indexName.classList.add("indexName"+i);
+    for (let i = 1; i <= 9; i++) {
+        var indexName = document.querySelector("#indexName" + i);
+        indexName.classList.add("indexName" + i);
     }
     var searchAll = document.querySelector("#searchAll");
     var filter = document.querySelector(".filter");
-   var  topp = bannerSearch.offsetTop-window.scrollY;
-    window.onscroll=()=>{
-        if(bannerSearch.offsetTop>window.scrollY){
-            searchAll.style.display="none";
+    var topp = bannerSearch.offsetTop - window.scrollY;
+    window.onscroll = () => {
+        if (bannerSearch.offsetTop > window.scrollY) {
+            searchAll.style.display = "none";
             filter.style.display = "none";
         }
-        else if(bannerSearch.offsetTop<window.scrollY+60){
-            searchAll.style.display="block";
+        else if (bannerSearch.offsetTop < window.scrollY + 60) {
+            searchAll.style.display = "block";
         }
-         topp = bannerSearch.offsetTop-window.scrollY+(window.scrollY/10);
+        topp = bannerSearch.offsetTop - window.scrollY + (window.scrollY / 17);
     }
-    bannerSearch.addEventListener("click",()=>{
-        filter.style.display="block";
-        filter.style.top=topp/6.5+"%";
+    bannerSearch.addEventListener("click", () => {
+        filter.style.display = "block";
+        filter.style.top = (topp/6.8)+ "%";
     })
-    
+
 })

b1a98736c2e382e2f7a12509f85c6b05b5713782
jinyeh0923 <lyrayeh0@gmail.com>
2019-01-22 23:28:03 +0800

    整理scss到一半 先commit

---
+++ js/journalsOverview.js
@@ -0,0 +1,9 @@
+$('.joForm__input').click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#' + $(e.target).attr('for')).prop('checked', true);
+});
+$(document).click(function () {
+    $('.joForm__input').removeClass('expanded');
+});

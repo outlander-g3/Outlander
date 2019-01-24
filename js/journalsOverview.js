f23a0fd10caddb7393a67a4ff134405a484d10cc
jinyeh0923 <lyrayeh0@gmail.com>
2019-01-24 22:45:36 +0800

    準備重新定位= .. =

--- js/journalsOverview.js
+++ js/journalsOverview.js
@@ -1,3 +1,4 @@
+//日誌篩選bar的js-------------------------
 $('.joForm__input').click(function (e) {
     e.preventDefault();
     e.stopPropagation();
@@ -7,5 +8,25 @@ $('.joForm__input').click(function (e) {
 $(document).click(function () {
     $('.joForm__input').removeClass('expanded');
 });
+// --------------------------------------
+
+
+//日誌收藏按鈕的js-------------------------
+$('.material-icons').click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('setFavorite');
+});
+$(document).click(function () {
+    $('.material-icons').removeClass('setFavorite');
+});
+// --------------------------------------
+
+//點擊山的圖片跳轉到下面日誌的js--------------
+$('.imgMount').click(function () {
+    $('html,body').animate({ scrollTop: $('.jo__filter__and__write').offset().top }, 400);
+});
+// --------------------------------------
+
 
 

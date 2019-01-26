e02ab30fe7e53875e571209f850d36c0c3277064
junyu831006 <junyu831006@gmail.com>
2019-01-26 21:45:36 +0800

    info切法難處理（暫時解決）

---
+++ js/journal.js
@@ -0,0 +1,40 @@
+/*用 O(id) 來取得getElementById 減少攏長*/ 
+function O(id){
+    return (typeof id == 'object'? i : document.getElementById(id));
+    }
+
+function jnInsertT(){
+
+    enquire.register("screen and (max-width: 767px)", {     
+        match: function() {
+                // JavaScript here
+                // 當CSS media query計算的視窗寬度小於769px時執行
+                // 網頁載入時執行一次
+                // 之後每次改變視窗時會再執行一次
+                var aaa=O('aaa');
+                aaa.style.position = 'static';
+                var bbb=O('jnContent__1thP');
+                jnContent.insertBefore(aaa,bbb);
+            },
+        });
+        enquire.register("screen and (min-width: 768px)", {     
+            
+            match: function() {
+            
+                // JavaScript here
+                // 當CSS media query計算的視窗寬度大於等於768px時執行
+                // 網頁載入時執行一次
+                // 之後每次改變視窗時會再執行一次
+                var aaa=O('aaa');
+                aaa.style.position = 'sticky';
+
+            },
+        });
+}
+
+function init(){
+
+}
+
+window.addEventListener('load',init);
+window.addEventListener('load',jnInsertT);

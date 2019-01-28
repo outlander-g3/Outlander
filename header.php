6af8474a867b02abd38fe407e8d0658f48246fd3
Elison Kee <chi811104@gmail.com>
2019-01-28 23:40:11 +0800

    header footer改ＰＨＰ插入

--- header.php
+++ header.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>山行者 - 結帳頁面</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/paid.css">
-    <script src="js/vue.min.js"></script>
-    <script src="js/header.js"></script>
-</head>
-
-<body>
-<?php
-        include_once('header.php');
-    ?>
 <nav id="headerAll">
         <div class="header">
             <ul id="logo">
@@ -78,43 +57,3 @@
             </div>
         </div>
     </nav>
-
-    <h2>訂購完成</h2>
-    <div class="pdPaid">
-        <p>感謝您此次的購買！！</p>
-        <p>前往<a href="">『訂單查詢』</a>查看訂單</p>
-        <p>前往<a href="">『裝備清單』</a>製作專屬清單</p>
-        <p>回到<a href="index.html">『山行者首頁』</a>繼續逛逛</p>
-    </div>
-    <img id="robot" src="img/暫時用的圖/robot-solid.png" alt="機器人">
-    <i id="up" class="material-icons">keyboard_arrow_up</i>
-    
-    <!-- <footer id="footer">
-        <img id="footer-img" src="img/暫時用的圖/foo1.png" alt="footer">
-        <div id="footer-all">
-            <ul>
-                <li>關於我們</li>
-                <li>聯繫我們</li>
-                <li>使用條款</li>
-                <li>尋找分店</li>
-            </ul>
-            <div id="footer-icon">
-                <span><i class="fab fa-facebook-f"></i>Facebook</span>
-                <span><i class="fab fa-instagram"></i>Instagram</span>
-                <span><i class="fab fa-twitter"></i>Twitter</span>
-            </div>
-            <div id="footer-input">
-                <div>
-                    <input type="text">
-                    <button>訂閱</button>
-                </div>
-                <span>COPYRIGHT © 2018 Outlander All rights reserved.</span>
-            </div>
-        </div>
-    </footer> -->
-    <?php
-        include_once('footer.php');
-    ?>
-</body>
-
-</html>

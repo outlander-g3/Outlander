1c8b1195c405a7eead889ff43b6489cf088eb18f
Elison Kee <chi811104@gmail.com>
2019-02-01 16:57:13 +0800

    蠻幹完了TAB帶資料 不會用vue啦ＱＱ

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,5 @@
+
+
 function $(id) {
     return document.getElementById(id);
 }
@@ -98,13 +100,15 @@ add.addEventListener('click', addPassanger, false);
 mi.addEventListener('click', miPassanger, false);
 
 function addPassanger() {
-    //乘客人數
+    //旅客人數
     if (count.innerHTML == 5) {
         return;
     }
     count.innerHTML = parseInt(count.innerHTML) + 1;
+    let label = document.createElement('label');
     let a = document.createElement('a');
     a.setAttribute('herf', 'javascript:;');
+    a.addEventListener('click', showFile, false);
     let div = document.createElement('div');
     div.setAttribute('class', 'row');
     let i1 = document.createElement('i');
@@ -113,10 +117,27 @@ function addPassanger() {
     let i2 = document.createElement('i');
     i2.innerText = count.innerHTML;
     detailCount.innerHTML = count.innerHTML;
+    let input1 = document.createElement('input');
+    input1.setAttribute('type', 'hidden');
+    input1.setAttribute('name', 'psgName');
+    let input2 = document.createElement('input');
+    input2.setAttribute('type', 'hidden');
+    input2.setAttribute('name', 'psgBd');
+    let input3 = document.createElement('input');
+    input3.setAttribute('type', 'hidden');
+    input3.setAttribute('name', 'psgId');
+    let input4 = document.createElement('input');
+    input4.setAttribute('type', 'hidden');
+    input4.setAttribute('name', 'psgTel');
     div.append(i1);
     div.append(i2);
+    div.append(input1);
+    div.append(input2);
+    div.append(input3);
+    div.append(input4);
     a.append(div);
-    passengerTab.append(a);
+    label.append(a);
+    passengerTab.append(label);
 
     //呼叫改變金額函數
     newTotal();
@@ -130,6 +151,27 @@ function miPassanger() {
 
     //呼叫改變金額函數
     newTotal();
+
+    //如果正在填寫的人被刪掉，資料欄位要改成前一個旅客
+    //正在填寫的人的位置 
+    var whoIsWritten = $('.who').find('i:nth-child(2)').text();
+    // console.log("現在在寫旅客：" + whoIsWritten);
+    var whoNo = document.querySelectorAll('.who').length;
+    // console.log("有幾個who：" + whoNo);
+    if (count.innerHTML < 5) {
+        if (whoNo < 1) {
+            $('.ctPassanger__tab label:last-child a').attr('class', 'who');
+        }
+        let nowName = $('.who').find("input[name='psgName']").val();
+        let nowBd = $('.who').find("input[name='psgBd']").val();
+        let nowId = $('.who').find("input[name='psgId']").val();
+        let nowTel = $('.who').find("input[name='psgTel']").val();
+        //顯示該人資料
+        $('#psgName').val(nowName);
+        $('#psgBd').val(nowBd);
+        $('#psgId').val(nowId);
+        $('#psgTel').val(nowTel);
+    }
 }
 
 var total = document.querySelector('.ctDetail__total--num');
@@ -164,11 +206,72 @@ for (let i = 0; i < btnPay.length; i++) {
             alert('請詳閱並同意訂購條款');
         }
         else {
-            // window.location = 'paid.php';
-            //跳出.ctPaid
-            // var winJump = document.querySelector('.ctPaid');
-            // winJump.style.display = 'block';
-            winJump('.btn-pay', '.ctPaid');
+            winJump_if(btnPay[i], '.ctPaid');
         }
     }, false);
 }
+
+
+
+var psgTabs = document.querySelectorAll('.ctPassanger__tab a');
+for (let i = 0; i < psgTabs.length; i++) {
+    psgTabs[i].addEventListener('click', showFile, false);
+}
+
+
+// 有input就塞進去
+function bring() {
+    // 把資料塞進去該tab到的旅客的值
+    //只有一個人的話，input值塞給第一個人的value
+    if (count.innerHTML == 1) {
+        // console.log($('#psgName').val());
+        $("input[name='psgName']").val($('#psgName').val());
+        $("input[name='psgBd']").val($('#psgBd').val());
+        $("input[name='psgId']").val($('#psgId').val());
+        $("input[name='psgTel']").val($('#psgTel').val());
+        // return;
+    }
+    //如果有兩個人以上,tab到誰,先顯示別人的value，有輸入的話塞給那個人的value
+    else {
+        // 值塞進去
+        let inputName = $('#psgName').val();
+        // console.log("現在的框框值:" + inputNow);
+        // 把現在框框值寫回去
+        // console.log($(this));
+        // $('.who').find("input[name='psgName']").attr('value', inputName);
+        $('.who').find("input[name='psgName']").val(inputName);
+        // return inputNow;
+        let inputBd = $('#psgBd').val();
+        $('.who').find("input[name='psgBd']").val(inputBd);
+        let inputId = $('#psgId').val();
+        $('.who').find("input[name='psgId']").val(inputId);
+        let inputTel = $('#psgTel').val();
+        $('.who').find("input[name='psgTel']").val(inputTel);
+
+    }
+}
+
+
+
+var psgName = document.querySelector('#psgName');
+function showFile(e) {
+    // 該人目前資料
+    $('.who').attr('class', '');
+    $(this).attr('class', 'who');
+    let nowName = $(this).closest('a').find("input[name='psgName']").val();
+    let nowBd = $(this).closest('a').find("input[name='psgBd']").val();
+    let nowId = $(this).closest('a').find("input[name='psgId']").val();
+    let nowTel = $(this).closest('a').find("input[name='psgTel']").val();
+    //顯示該人資料
+    $('#psgName').val(nowName);
+    $('#psgBd').val(nowBd);
+    $('#psgId').val(nowId);
+    $('#psgTel').val(nowTel);
+    // console.log(now);
+}
+//每個input註冊輸入事件
+var inputs = document.querySelectorAll('.ctPassanger__input input');
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', bring, false);
+}
+
--- js/common.js
+++ js/common.js
@@ -1,11 +1,16 @@
 // 跳窗
-//按下會有跳窗的函數 win(被點擊的按鈕,要跳出的視窗)
+//按下會有跳窗的函數 winJump(被點擊的按鈕,要跳出的視窗)
 //例如我按下 .btn-pay按鈕  .ctPaid要出現
 // 就在我那支js寫完按鈕的addEvevntListener的function呼叫winJump('.btn-pay','.ctPaid');
+function winJump_if(btn, win) {
+    var winJump = document.querySelector(win);
+    winJump.style.display = 'block';
+}
+
 function winJump(btn, win) {
-    var Btn = document.querySelectorAll(btn);
-    for (let i = 0; i < Btn.length; i++) {
-        Btn[i].addEventListener('click', function () {
+    var btns = document.querySelectorAll(btn);
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function () {
             var winJump = document.querySelector(win);
             winJump.style.display = 'block';
         }, false);

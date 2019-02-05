b2ba5051f33a1c15661d44fe8cf236a2af88b787
yangwuhsu <fuhui1104ttw@gmail.com>
2019-02-05 20:46:13 +0800

    revise 2 edition

---
+++ js/robot.php
@@ -0,0 +1,51 @@
+<button class="rbcircle" type="image" src="img/機器人/robot-solid .png">
+        <span class="rbicon-elements">
+        </span>
+    </button>
+
+    <div class="rbmodal-wrapper" id="model">
+        <div class="rbhead">
+            <div class="rbhead__box">
+                <div class="rblogo__box">
+                    <div class="rblogo"><img src="img/logo.png" alt="logo"></div>
+                    <h2>山行者</h2>
+                </div>
+                <div class="rbclose"><img class="rbout" id="out" src="img/機器人/baseline_close_white_24dp.png" alt="close">
+                <img class="rbclose2 rbclose" id="out1" src="img/機器人/baseline_keyboard_arrow_left_white_24dp.png" alt=""></div>
+            </div>
+        </div>
+
+        <div id="rb__message">
+            <div class="rbtext__show">
+                <img src="img/logo.png">
+                <p>您好,請問需要幫忙嗎?</p><br/>
+            </div>
+        </div>
+        <div class="rbinput__text">
+            <div class="rbtext__input">
+                <textarea id="say" name="say" type="text" cols="1" rows="1"></textarea>
+                <input class="rbbutn" type="image" id="input" onclick="say()" src="img/機器人/baseline_send_black_24dp.png" />
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- 向上按鈕 -->
+    <!-- <i id="up" class="material-icons">keyboard_arrow_up</i> -->

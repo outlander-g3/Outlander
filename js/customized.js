c1999c1ed545358eff94f3048fed9e85c7c7ffe0
junyu831006 <junyu831006@gmail.com>
2019-02-10 11:51:09 +0800

    counting quan DONE

--- js/customized.js
+++ js/customized.js
@@ -166,6 +166,149 @@ function backPickSc(){
             },
         });
 }
+
+
+/*****  計算價格數量及動態新增刪除到【我的風景路線規劃】  *****/ 
+var storage = sessionStorage;
+
+// alert();
+function addItem(itemId,itemValue){	
+    //風景列表物件代號
+    var cu_IDNum = itemValue.split('|')[3];
+    var cu_ID =O(cu_IDNum);
+   
+    //新增風景最大容器
+    var cuCustom__dropSceneryNew = document.createElement('div');
+    //加A是為了與上片的cu_ID做區別
+    // cuCustom__dropSceneryNew.id=cu_IDNum+"A";
+    cuCustom__dropSceneryNew.classList.add("cuCustom__dropSceneryNew");
+    cuCustom__dropSceneryNew.setAttribute("draggable","true");
+    cuCustom__dropSceneryNew.style.backgroundImage = ' url(../img/'+itemValue.split('|')[1]+')';
+
+	var cuCustom__sceneryContent= document.createElement('div');
+    cuCustom__sceneryContent.classList.add("cuCustom__dropScenery&#x20;div:first-child");
+    
+    var title = document.createElement('p');
+	title.innerText = itemValue.split('|')[0];
+
+
+	var price = document.createElement('span');
+    price.innerText = itemValue.split('|')[2];
+    price.classList.add('cuCustom__price');
+    
+  
+    var cu_mIconRestaurant = document.createElement('i');
+    cu_mIconRestaurant.classList.add('material-icons');
+    cu_mIconRestaurant.classList.add('.cuCustom__dropScenery&#x20;div:first-child&#x20;i');
+    cu_mIconRestaurant.innerText = 'restaurant';
+    
+    var cu_fIconCampground = document.createElement('i');
+    cu_fIconCampground.classList.add('fas');
+    cu_fIconCampground.classList.add('fa-campground');
+    cu_fIconCampground.classList.add('.cuCustom__dropScenery&#x20;div:first-child&#x20;i');
+
+    
+    //刪除按鈕
+    var btn_cuIconClear = document.createElement('button');
+    btn_cuIconClear.classList.add('btn_cuIcon--clear');
+    btn_cuIconClear.classList.add('cuBtn__styleClear');
+    btn_cuIconClear.id = itemId;
+    var cu_mIconClear = document.createElement('i');
+    cu_mIconClear.classList.add('material-icons');
+    cu_mIconClear.innerText = 'clear'; 
+    
+    
+    
+	//先判斷此處是否已有物件，如果有就先刪除
+	// if(cuCustom__dropMask.hasChildNodes()){
+        //     	while(cuCustom__dropMask.childNodes.length >= 1){
+            //         		cuCustom__dropMask.removeChild(cuCustom__dropMask.firstChild)
+            //         	}
+            //         }	
+            
+    //再顯示新物件
+    var cuCustom__dropMask = document.getElementsByClassName('cuCustom__dropMask')[0];
+    cuCustom__dropMask.insertBefore(cuCustom__dropSceneryNew, cuCustom__dropMask.childNodes[0]);
+    cuCustom__dropSceneryNew.appendChild(cuCustom__sceneryContent);
+    cuCustom__dropSceneryNew.appendChild(price);
+    cuCustom__dropSceneryNew.appendChild(btn_cuIconClear);
+    cuCustom__sceneryContent.appendChild(title);
+    cuCustom__sceneryContent.appendChild(cu_mIconRestaurant);
+    cuCustom__sceneryContent.appendChild(cu_fIconCampground);
+    btn_cuIconClear.appendChild(cu_mIconClear);
+
+	//存入storage
+	if(storage[itemId]){
+        // alert('You have checked.');
+	}else{
+        storage['addItemList'] += itemId + ', ';
+		storage[itemId] = itemValue;
+	}
+
+
+    var cuCustom__sceneryZoneOF = O('cuCustom__sceneryZone--OF');
+    cuCustom__sceneryZoneOF.removeChild(cu_ID);
+
+    var btn_cuDelete= cuCustom__dropSceneryNew.appendChild(btn_cuIconClear);
+    btn_cuDelete.addEventListener('click',deleteItem);
+    btn_cuDelete.addEventListener('click',changeItemCount);
+    function deleteItem(){
+        var itemId = this.getAttribute('id');
+        // 刪除該筆資料之前:
+        // 1.先將總金額(total)扣除
+        var itemValue = storage.getItem(itemId);
+        // console.log(itemValue);
+        cuAmount -= parseInt(itemValue.split('|')[2]);
+
+        document.getElementById('cuAmount').innerText = cuAmount;
+
+        // 2.清除storage的資料
+        storage.removeItem(itemId);
+        storage['addItemList'] = storage['addItemList'].replace(itemId+', ','');
+        // storage.setItem('addItemList',storage.getItem('addItemList').replace(itemId+', ',''));
+
+        // 3.再將該筆tr刪除
+        cuCustom__dropMask.removeChild(cuCustom__dropSceneryNew);
+        cuCustom__sceneryZoneOF.insertBefore(cu_ID,cuCustom__sceneryZoneOF.childNodes[0]);
+    }
+
+    function changeItemCount(){
+        var itemString = storage.getItem('addItemList');
+        // var itemStringa = storage.removeItem('addItemList');     
+        var items = itemString.substr(0,itemString.length-2).split(', ');
+ 
+        if(items == ""){
+            items.length -= 1;
+            O('cuQuan').innerText = items.length;
+        }
+        O('cuQuan').innerText = items.length;
+    }
+
+
+    
+	// // 計算購買數量和小計
+    var itemString = storage.getItem('addItemList');
+    // var itemStringa = storage.removeItem('addItemList');
+   
+    var items = itemString.substr(0,itemString.length-2).split(', ');
+   
+    
+	cuAmount = 0;
+	for(var key in items){		//use items[key]
+		var itemInfo = storage.getItem(items[key]);
+		var itemPrice = parseInt(itemInfo.split('|')[2]);        
+		cuAmount += itemPrice;
+	}
+
+	O('cuQuan').innerText = items.length;
+    O('cuAmount').innerText = cuAmount;
+}
+
+
+
+
+
+
 function init(){
     // var cuProcess2 = O('cuProcess2');
     cuProcess1.addEventListener('click',cuSlide1);
@@ -218,6 +361,19 @@ function init(){
     var btnCuBack=O('btn_cuBack');
     btnCuBack.addEventListener('click',backPickSc);
     // alert(123);
+
+    if(storage['addItemList'] == null){
+		storage['addItemList'] = '';	 //storage.setItem('addItemList','');
+	}
+
+    //幫每個Add Cart建事件聆聽功能
+	var list = document.querySelectorAll('.btn_cuAddScenery');  //list是陣列
+	for(var i=0; i<list.length; i++){
+        list[i].addEventListener('click', function(){
+            var cuSceneryInfo = document.querySelector('#'+this.id+' input').value;
+            addItem(this.id,cuSceneryInfo);
+		});
+	}
 }
 
 window.addEventListener("load",init);

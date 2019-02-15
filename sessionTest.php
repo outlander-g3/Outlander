0f24e5db84e5e9c579f7ddc5b2389e6a548b05e2
yangwuhsu <fuhui1104ttw@gmail.com>
2019-02-15 15:39:10 +0800

    加入php

---
+++ sessionTest.php
@@ -0,0 +1,60 @@
+<?php
+    // include_once('connectDb.php'); //連線
+    // include_once('login.php'); //會員登入
+
+    //要去抓選擇的開團資訊（圖,名稱,日期
+
+    //抓
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>山行者 - PHP模板</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/cart.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/vue.min.js"></script>
+    <script src="js/enquire.min.js"></script>
+    
+    <!-- css塞這 自己js塞屁股 -->
+    
+    
+</head>
+
+<body>
+
+<!-- 插入header -->
+    <?php
+        include_once('header.php');
+    ?>
+
+<!-- ===========================各分頁內容開始======================= -->
+
+
+    <?
+        echo $_SESSION['memName'];
+    ?>
+
+<!-- ===========================各分頁內容結束======================= -->
+<!-- 插入 footer 會員登入跟機器人 -->
+<?php
+    include_once('footer.php');
+    // include_once('robot.php');
+    include_once('memLogin.php');
+?>
+</body>
+</html>
+
+
+<script src="js/common.js"></script>
+<script src="js/header.js"></script>
+<!-- <script src="js/robot.js"></script> -->

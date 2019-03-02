6228528e535e11e3827f4d6dde66be6dff9eeed9
junyu831006 <junyu831006@gmail.com>
2019-03-02 18:03:52 +0800

    撈嚮導成功

--- cu_getGuide.php
+++ cu_getGuide.php
@@ -7,32 +7,42 @@ try {
     $cuEnd=$_GET['cuEnd'];
     $_SESSION['pdStart']=$cuStart;
     $_SESSION['pdEnd']=$cuEnd;
-    echo $cuStart;
-    echo $cuEnd;
+    // echo $cuStart;
+    // echo $cuEnd;
     
     
     //撈嚮導
     require_once("connectDb.php");
     // $sql="select a.pdNO ,a.gdNo1,a.gdNo2 from product a , guide b where a.pdStart BETWEEN 'cuStart=:cuStart' AND 'cuEnd=:cuEnd' AND a.gdNo1 >15 or a.gdNo2>15  ";
-    $sql="select distinct a.pdNO ,a.gdNo1,a.gdNo2 from product a , guide b where a.pdStart BETWEEN :cuStart AND :cuEnd or a.gdNo1 >15";
+    $sql="select distinct a.pdNO ,a.gdNo1,a.gdNo2 from product a , guide b where a.pdStart BETWEEN :cuStart AND :cuEnd AND a.gdNo1 >15";
     // $sql="select a.pdNO ,a.gdNo1,a.gdNo2,a.pdStart from product a , guide b where a.pdStart BETWEEN '2019-03-13' AND '2019-03-17' AND a.gdNo1 >15";
     $scnG=$pdo->prepare($sql);
     $scnG->bindValue(':cuStart',$cuStart);
     $scnG->bindValue(':cuEnd',$cuEnd);
     $scnG->execute();  
     
-    echo $sql;
    
-
-    //當日有帶團之嚮導的小山屋
-    $arr =[];
-    while($scnGRows = $scnG->fetch(PDO::FETCH_ASSOC)){
-     
-        array_push($arr, $scnGRows['gdNo1'],$scnGRows['gdNo2']); 
-    }
+   
+ 
+    if($scnG->rowCount()==0){
+        $sql ="select distinct gdName ,gdImg ,skill,gdNo from guide where gdNo>20";
+        $scnG3=$pdo->prepare($sql);
+        $scnG3->execute();
+        while($scnGRow3s = $scnG3->fetch(PDO::FETCH_ASSOC)){
+            echo '<div class="cu__guideItem cu__guideItem1">
+                    <img src="img/guide/',$scnGRow3s['gdImg'],'" alt="嚮導照片">
+                    <input type="hidden" value="',$scnGRow3s['gdName'],'|',$scnGRow3s['skill'],'|',$scnGRow3s['gdNo'],'">
+                </div>  ';
+        }
+    }else if($scnG->rowCount()!=0){   
+        
+        //當日有帶團之嚮導的小山屋
+        $arr =[];
+                while($scnGRows = $scnG->fetch(PDO::FETCH_ASSOC)){
+                    array_push($arr, $scnGRows['gdNo1'],$scnGRows['gdNo2']); 
+                }
     //刪除重複的值
     $arrR =array_unique($arr);
-    print_r($arrR);
 
     //給編號21-25的嚮導編號
     $arr2 =[];
@@ -54,7 +64,6 @@ try {
 
     //將可帶團
     $values = implode(",",array_unique($temparray));
-    echo $values;
     $sql ="select gdName ,gdImg ,skill,gdNo from guide where gdNo in ($values)";
     $scnG2=$pdo->prepare($sql);
     $scnG2->execute();
@@ -64,6 +73,7 @@ try {
                 <input type="hidden" value="',$scnGRow2s['gdName'],'|',$scnGRow2s['skill'],'|',$scnGRow2s['gdNo'],'">
             </div>  ';
     }
+}
 
 } catch (PDOException $e) {
     echo "失敗",$e->getMessage(),"<br>";
